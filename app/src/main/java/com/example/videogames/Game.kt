66688fb30180b2bf3bc2d297a6cb9e16523ca30c
epeljto1 <epeljto1@etf.unsa.ba>
2023-03-29 23:02:02 +0200

    cardview fix

--- app/src/main/java/com/example/videogames/Game.kt
+++ app/src/main/java/com/example/videogames/Game.kt
@@ -5,4 +5,10 @@ data class Game(
     val platform: String,
     val releaseDate: String,
     val rating: Double,
+    val coverImage: String,
+    val esrbRating: String,
+    val developer: String,
+    val publisher: String,
+    val genre: String,
+    val description: String,
 )

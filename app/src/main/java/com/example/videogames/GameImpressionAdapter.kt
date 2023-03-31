4192321a9e9e5517890c9f8b82ffc24fbb344698
epeljto1 <epeljto1@etf.unsa.ba>
2023-04-01 01:24:24 +0200

    game impression adapter

--- app/src/main/java/com/example/videogames/GameImpressionAdapter.kt
+++ app/src/main/java/com/example/videogames/GameImpressionAdapter.kt
@@ -11,13 +11,20 @@ class GameImpressionAdapter(
     private var impressions: List<UserImpression>
 ) : RecyclerView.Adapter<GameImpressionAdapter.ViewHolder>(){
 
+
     override fun getItemViewType(position: Int): Int {
         return if (impressions[position] is UserRating) 0 else 1
     }
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    class ViewHolder(view: View, viewType: Int): RecyclerView.ViewHolder(view) {
         val username_textview: TextView
+        lateinit var review_textview: TextView
+        lateinit var rating_bar: RatingBar
         init {
             username_textview=view.findViewById(R.id.username_textview)
+            if(viewType==1)
+                review_textview = view.findViewById(R.id.review_textview)
+            else rating_bar = view.findViewById((R.id.rating_bar))
 
         }
     }
@@ -26,17 +33,35 @@ class GameImpressionAdapter(
         val layout=if(viewType==0) R.layout.rating_layout else R.layout.review_layout
         val view=LayoutInflater.from(viewGroup.context)
             .inflate(layout,viewGroup,false)
-        return ViewHolder(view)
+        return ViewHolder(view,viewType)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         viewHolder.username_textview.text=impressions[position].username
+        if(impressions[position] is UserReview)
+            viewHolder.review_textview.text=getReview(impressions[position])
+        else
+            viewHolder.rating_bar.rating=getRating(impressions[position]).toFloat()
+
     }
     override fun getItemCount():Int = impressions.size
 
     fun updateImpressions(impressions: List<UserImpression>) {
         this.impressions = impressions
         notifyDataSetChanged()
+    }
+
+    private fun getReview(impression: UserImpression): String {
+        lateinit var Review : UserReview
+            Review = impression as UserReview
+        return Review.review
+    }
+
+    private fun getRating(impression: UserImpression): Double
+    {
+        lateinit var Rating : UserRating
+        Rating = impression as UserRating
+        return Rating.rating
     }
 
 }

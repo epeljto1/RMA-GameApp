3d8cd223e3adb5cbe022edc048efb01991da2851
epeljto1 <epeljto1@etf.unsa.ba>
2023-04-27 19:38:31 +0200

    espresso tests 1 and 2

--- app/src/androidTest/java/com/example/videogames/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/videogames/ExampleInstrumentedTest.kt
@@ -1,12 +1,11 @@
 package com.example.videogames
 
 
-import android.view.View
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.NoMatchingViewException
-import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.PositionAssertions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,12 +18,15 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class TestLayout {
-
-
+class OwnEspressoTests {
     @get:Rule
     var homeRule:ActivityScenarioRule<HomeActivity> = ActivityScenarioRule(HomeActivity::class.java)
 
+    // Prvi scenarij Test1() provjerava ispravnost aplikacije kada se ona tek pokrene
+    // Testira se da li je Home fragment vidljiv i da li aplikacija ima bottom navigation
+    // Takodjer se testira da li taj bottom navigation ima dvije opcije - Home i Details
+    // i da li je bottom navigation disabled jer jos nije kliknuto na neku igricu
+    // Sve sto se testira i kako test provjerava navedeno se nalazi unutar njega uz komentare
     @Test
     fun Test1(){
 
@@ -46,10 +48,47 @@ class TestLayout {
         onView(withId(R.id.gameDetailsItem)).check(matches(not(isEnabled())))
     }
 
+    // Drugi scenarij Test2() provjerava ispravnost aplikacije nakon klika na neku igricu
+    // Testira se da li je Game Details fragment vidljiv i da li su prikazani detalji igrice na koju je kliknuto
+    // Provjerava se da li je bottom navigation sada enabled i da li se klikom na Home opciju vraca na Home fragment
+    // Zatim se provjerava klik na opciju Details i da li se ponovo otvaraju detalji igrice na koju je posljednje kliknuto
+    // Sve sto se testira i kako test provjerava navedeno se nalazi unutar njega uz komentare
+    @Test
     fun Test2(){
 
-    }
+        // Provjera da li se nakon klika na igricu otvaraju detalji o igrici
+        var igra = GameData.getAll()[1]
+        onView(withId(R.id.game_list)).perform(RecyclerViewActions.actionOnItem<GameListAdapter.ViewHolder>(allOf(
+            hasDescendant(withText(igra.title)),
+            hasDescendant(withText(igra.releaseDate)),
+            hasDescendant(withText(igra.rating.toString()))
+        ),click()))
 
+        onView(withId(R.id.gameDetailsItem)).check(matches(isDisplayed()))
+        onView(withId(R.id.game_title_textview)).check(matches(isDisplayed()))
+
+        // Provjera da li su se otvorili ispravni detalji
+        var title1 = onView(withId(R.id.game_title_textview))
+        title1.check(matches(withText(igra.title)))
+        onView(withId(R.id.cover_imageview)).check(matches(isDisplayed()))
+        onView(withId(R.id.description_textview)).check(matches(isDisplayed()))
+
+        // Provjera da li je traka za navigaciju enabled nakon klika na igricu
+        onView(withId(R.id.homeItem)).check(matches(isEnabled()))
+
+        // Provjera da li nakon klika na opciju Home se prikaze Home fragment
+        onView(withId(R.id.homeItem)).perform(click())
+        onView(withId(R.id.homeItem)).check(matches(isDisplayed()))
+
+        // Provjera da li nakon klika na opciju Details se prikaze ponovo Details fragment
+        onView(withId(R.id.gameDetailsItem)).perform(click())
+        onView(withId(R.id.gameDetailsItem)).check(matches(isDisplayed()))
+
+        // Provjera da li su se ponovo otvorili ispravni detalji
+        var title2 = onView(withId(R.id.game_title_textview))
+        title2.check(matches(withText(igra.title)))
+
+    }
 
 
 }
